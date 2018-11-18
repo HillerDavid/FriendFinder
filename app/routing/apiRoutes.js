8cc336ac060199c1eda1176194cad1cfe6e53a33
David Hiller <HillerDavid@users.noreply.github.com>
2018-11-18 12:35:47 -0500

    Added html routes.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,8 @@
+
+
+//Route to display JSON
+module.exports = (app) {
+    
+}
+
+//Route handling incoming survey results
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+let path = require('path')
+
+module.exports = (app) => {
+
+    //Route that leads to home page
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '..public/home.html'))
+    })
+
+    //Route to display survey page
+    app.get('/survey', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/survey.html'))
+    })
+
+}
